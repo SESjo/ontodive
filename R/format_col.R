1c430d7b7ae66da497003dd507ebe3a40ea59a6a
Joffrey JOUMAA <joffreyjoumaa@icloud.com>
2022-03-31 14:51:51 -0700

    reformat + trick to avoid error when checking

--- R/format_col.R
+++ R/format_col.R
@@ -1,6 +1,6 @@
-#' Format
+#' @title Format columns
 #'
-#' Format columns of a dataset by removing capital letters, bracket and
+#' @description Format columns of a dataset by removing capital letters, bracket and
 #' transform white space as underscore
 #'
 #' @param col_names A vector of character containing column names to be
@@ -12,19 +12,20 @@
 #' @importFrom stringr str_replace_all str_to_lower str_replace_all
 #' @examples
 #' # format several vectors of characters
-#' format_col(c("lkjez alzkj","kjfe[lkjz]","lkjee [lkj]"))
-#'
+#' format_col(c("lkjez alzkj", "kjfe[lkjz]", "lkjee [lkj]"))
 format_col <- function(col_names) {
   # remove white space
-  col_names_trim = str_replace_all(string = col_names,
-                                   pattern = " ",
-                                   replacement = "")
+  col_names_trim <- str_replace_all(
+    string = col_names,
+    pattern = " ",
+    replacement = ""
+  )
   # convert upper case to lower case
-  col_names_trim_lower = str_to_lower(col_names_trim)
+  col_names_trim_lower <- str_to_lower(col_names_trim)
   # replace "[" by "_"
-  col_names_trim_lower_bracket_inter = str_replace_all(col_names_trim_lower, "\\[", "_")
+  col_names_trim_lower_bracket_inter <- str_replace_all(col_names_trim_lower, "\\[", "_")
   # replace "]" by ""
-  col_names_trim_lower_bracket = str_replace_all(col_names_trim_lower_bracket_inter, "\\]", "")
+  col_names_trim_lower_bracket <- str_replace_all(col_names_trim_lower_bracket_inter, "\\]", "")
   # return
   return(col_names_trim_lower_bracket)
 }
