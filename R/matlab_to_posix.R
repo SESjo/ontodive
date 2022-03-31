523719830e96fe1f9df37ff6a336da79c46f038d
Joffrey JOUMAA <joffreyjoumaa@icloud.com>
2022-03-31 16:31:53 -0700

    reformat

--- R/matlab_to_posix.R
+++ R/matlab_to_posix.R
@@ -1,6 +1,6 @@
-#' Convert Matlab datenum to POSIX
+#' @title Convert Matlab datenum to POSIX
 #'
-#' Small function to convert Matlab datenum to POSIX time
+#' @description Small function to convert Matlab datenum to POSIX time
 #'
 #' @param x column with Matlab datenum
 #' @param timez time zone to be used
@@ -11,17 +11,18 @@
 #' \href{http://lukemiller.org/index.php/2011/02/converting-matlab-and-r-date-and-time-values/}{http://lukemiller.org/index.php/2011/02/converting-matlab-and-r-date-and-time-values/}
 #'
 #' @examples matlab_to_posix(c(737182.4))
-#'
-matlab_to_posix = function(x, timez = "UTC") {
-  days = x - 719529 	# 719529 = days from 1-1-0000 to 1-1-1970
-  secs = days * 86400 # 86400 seconds in a day
+matlab_to_posix <- function(x, timez = "UTC") {
+  days <- x - 719529 # 719529 = days from 1-1-0000 to 1-1-1970
+  secs <- days * 86400 # 86400 seconds in a day
 
   return(as.POSIXct(
     strftime(
-      as.POSIXct(secs, origin = '1970-1-1',
-                 tz = 'UTC'),
-      format = '%Y-%m-%d %H:%M:%S',
-      tz = 'UTC',
+      as.POSIXct(secs,
+        origin = "1970-1-1",
+        tz = "UTC"
+      ),
+      format = "%Y-%m-%d %H:%M:%S",
+      tz = "UTC",
       usetz = FALSE
     ),
     tz = timez
