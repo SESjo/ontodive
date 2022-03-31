26a782b6c30d60a4e8069aabf414f13cdcd85686
Joffrey JOUMAA <joffreyjoumaa@icloud.com>
2022-03-31 14:45:40 -0700

    reformat + packages import in the example

--- R/check_dt.R
+++ R/check_dt.R
@@ -1,8 +1,8 @@
-#' Check if an object is a `data.table`
+#' @title Check if an object is a `data.table`
 #'
-#' Allow to quickly check if an object is a `data.table`
+#' @description Allow to quickly check if an object is a `data.table`
 #'
-#' This function checks if the class `data.table` is among the considered object's classes.
+#' @details This function checks if the class `data.table` is among the considered object's classes.
 #'
 #' @param dt The object to check
 #'
@@ -17,13 +17,15 @@
 #' @importFrom data.table data.table
 #'
 #' @examples
+#' # load data.table
+#' library(data.table)
+#'
 #' # data.table creation
 #' dt <- data.table()
 #'
 #' # check if `dt` is a data.table
 #' check_dt(dt)
-#'
-check_dt = function(dt){
+check_dt <- function(dt) {
   # return
   return(any(class(dt) == "data.table"))
 }
