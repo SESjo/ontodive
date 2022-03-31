28363b16ac8ae1415a53a95ddb8aa9810fe47696
Joffrey JOUMAA <joffreyjoumaa@icloud.com>
2022-03-31 14:42:14 -0700

    reformat + packages import + trick to avoid error when checking

--- R/build_github_vignette.R
+++ R/build_github_vignette.R
@@ -1,10 +1,22 @@
-#' Build Vignettes for Github
+#' @title Build Vignettes for Github
 #'
-#' This function aims at processing vignette (building, organizing) before pushing on Github so that user can download a package with pre-compiled vignette. Using the website argument allow to build the associated website.
+#' @description  function aims at processing vignette (building, organizing)
+#' before pushing on Github so that user can download a package with
+#' pre-compiled vignette. Using the website argument allow to build the
+#' associated website.
 #'
-#' To make sure pre-compiled vignette are enclosed within the package downloaded from Github, vignette are (i) compiled with tools::buildVignettes, (ii) then copy-paste to `inst\\doc`. To build the website, this function first converts README.Rmd file to README.md file, and then calls pkgdown::build_site() since this function only take into account *.md.
+#' @details To make sure pre-compiled vignette are enclosed within the package
+#' downloaded from Github, vignette are (i) compiled with tools::buildVignettes,
+#' (ii) then copy-paste to `inst\\doc`. To build the website, this function
+#' first converts README.Rmd file to README.md file, and then calls
+#' pkgdown::build_site() since this function only take into account *.md.
 #'
-#' @param website A boolean to compile the website associated with weanlingNES package
+#' @import dplyr
+#' @import pkgdown
+#'
+#' @param article The name or the path to the .Rmd file to be compiled
+#' @param website A boolean to compile the website associated with the article
+#' @param vignette A boolean to compile the vignette associated with the article
 #'
 #' @return A `\\docs` folder containing the website and a `inst\\doc` folder containing the vignette
 #'
@@ -15,22 +27,39 @@
 #'
 #' @examples
 #' \dontrun{
-#' # compile vignette
+#' # compile all articles into vignette and website
 #' build_github_vignette()
 #' }
 build_github_vignette <- function(article = NULL,
                                   website = NULL,
                                   vignette = NULL) {
+  # to avoid warnings when checking the package
+  # https://www.r-bloggers.com/2019/08/no-visible-binding-for-global-variable/
+  . <- NULL
+
+  # if "article" is not only the name of a file
+  # (e.g with extension ".Rmd" or path "/")
+  if (grepl(".Rmd|/", article)) {
+    # split the character chain with "/"
+    article <- strsplit(article,"/") %>%
+      # select the last element of the list
+      dplyr::last(.) %>%
+      # select the last item of this last element
+      dplyr::last(.) %>%
+      # remove ".Rmd"
+      sub(".Rmd", "", .)
+  }
+
   # if nothing is specified then build vignette and website
-  if (is.null(website) & is.null(vignette)){
-    website = TRUE
-    vignette = TRUE
+  if (is.null(website) & is.null(vignette)) {
+    website <- TRUE
+    vignette <- TRUE
   }
 
   # check is their is a name in the article argument
   if (is.null(article)) {
     # we rebuild the all documentation (vignette + website)
-    if (website == TRUE) {
+    if (!is.null(website) && website == TRUE) {
       # update favicon
       pkgdown::build_favicons(pkg = ".", overwrite = TRUE)
       # "re"initiate a website
@@ -50,7 +79,7 @@ build_github_vignette <- function(article = NULL,
       overwrite = TRUE
       )
     }
-    if (vignette == TRUE){
+    if (!is.null(vignette) && vignette == TRUE) {
       # build vignette
       tools::buildVignettes(
         dir = ".",
@@ -70,11 +99,12 @@ build_github_vignette <- function(article = NULL,
       )
     }
   } else {
-    if (website == TRUE) {
+    if (!is.null(website) && website == TRUE) {
       # update the article
       pkgdown::build_article(article)
     }
-    if (vignette == TRUE){
+
+    if (!is.null(vignette) && vignette == TRUE) {
       # update the vignette
       tools::buildVignette(
         file = paste0("./vignettes/", article, ".Rmd"),
@@ -86,9 +116,11 @@ build_github_vignette <- function(article = NULL,
         "vignettes",
         recursive = TRUE,
         full.names = TRUE,
-        pattern = c(paste0(article, ".html$|",
-                           article, ".Rmd$|",
-                           article, "*.R$"))
+        pattern = c(paste0(
+          article, ".html$|",
+          article, ".Rmd$|",
+          article, "*.R$"
+        ))
       ),
       "inst/doc",
       overwrite = TRUE
