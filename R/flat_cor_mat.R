0a9ca558e17808ea4e305d01d80862769e90b61d
Joffrey JOUMAA <joffreyjoumaa@icloud.com>
2022-03-31 14:50:54 -0700

    reformat documentation

--- R/flat_cor_mat.R
+++ R/flat_cor_mat.R
@@ -1,6 +1,6 @@
-#' Flatten correlation matrix
+#' @title Flatten correlation matrix
 #'
-#' Flatten a correlation matrix into a 2D dataframe.
+#' @description Flatten a correlation matrix into a 2D dataframe.
 #'
 #' @param cormat A correlation matrix
 #' @param pmat A p-value matrix
@@ -15,23 +15,22 @@
 #' \href{http://www.sthda.com/french/wiki/matrice-de-correlation-formattage-et-visualisation}{http://www.sthda.com/french/wiki/matrice-de-correlation-formattage-et-visualisation}
 #'
 #' @examples # Dataset generation
-#' data_test = data.frame(sapply(c(1:10),function(x) rnorm(10)))
+#' data_test <- data.frame(sapply(c(1:10), function(x) rnorm(10)))
 #'
 #' # Correlation matrix calculation
-#' cor_test = cor(data_test)
+#' cor_test <- cor(data_test)
 #'
 #' # Associated p-values
-#' cor_test_p = ggcorrplot::cor_pmat(data_test)
+#' cor_test_p <- ggcorrplot::cor_pmat(data_test)
 #'
 #' # Flatten the correlation matrix
 #' flat_cor_mat(cor_test, cor_test_p)
-#'
 flat_cor_mat <- function(cormat, pmat) {
   ut <- upper.tri(cormat)
   data.table(
     row = rownames(cormat)[row(cormat)[ut]],
     column = rownames(cormat)[col(cormat)[ut]],
-    cor  =(cormat)[ut],
+    cor = (cormat)[ut],
     p = pmat[ut]
   )
 }
